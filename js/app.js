9d5b84636c0a1bea193d72237465c002d1466328
Niklas Riekenbrauck <nikriek@gmail.com>
2016-02-27 14:32:42 +0000

    Add test code

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// Enable pusher logging - don't include this in production
+Pusher.log = function(message) {
+  if (window.console && window.console.log) {
+    window.console.log(message);
+  }
+};
+
+var pusher = new Pusher('63d59e4d863d6c327df0', {
+  encrypted: true
+});
+
+var channel = pusher.subscribe('global');
+channel.bind('music_keystroke', function(data) {
+  alert(data.message);
+});

c3fb21371eb5badf2df09e9d5a7222fffd34976c
NicoKnoll <mail@nico.is>
2016-02-27 16:09:48 +0100

    pusher unlimited

--- js/app.js
+++ js/app.js
@@ -6,31 +6,28 @@ Pusher.log = function(message) {
 };
 
 
-$(document).ready(function(){
-
-
-	var pusher = new Pusher('63d59e4d863d6c327df0', {
-	  encrypted: true,
-	  authTransport: 'client',
-	    clientAuth: {
-	      key: '63d59e4d863d6c327df0',
-	      secret: '3f3f9b3c99eaa688e16b',
-	      user_id: 1,
-	      user_info: {}
-	    }
-	});
+var pusher = new Pusher('63d59e4d863d6c327df0', {
+  encrypted: true,
+  authTransport: 'client',
+    clientAuth: {
+      key: '63d59e4d863d6c327df0',
+      secret: '3f3f9b3c99eaa688e16b',
+      user_id: 1,
+      user_info: {}
+    }
+});
 
 
 
-	var channel = pusher.subscribe('private-global');
-	channel.bind('music_keystroke', function(data) {
-		t = new track();
-		t.beat(4);
-	});
+var channel = pusher.subscribe('private-global');
+channel.bind('music_keystroke', function(data) {
+	console.log(data);
+	t = new track();
+	t.beat(4);
+});
 
-	$(document).keypress(function(event) {
-		channel.trigger('client-music_keystroke', {
-		  "message": "hello world"
-		});
+$(document).keypress(function(event) {
+	channel.trigger('client-music_keystroke', {
+	  "message": "hello world"
 	});
 });

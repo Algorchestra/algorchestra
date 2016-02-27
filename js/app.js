31b739ceaa772a1e1e37c3651b4b145d6a77cef2
NicoKnoll <mail@nico.is>
2016-02-27 16:10:54 +0100

    pusher unlimited

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ var pusher = new Pusher('63d59e4d863d6c327df0', {
 
 
 var channel = pusher.subscribe('private-global');
-channel.bind('music_keystroke', function(data) {
+channel.bind('client-music_keystroke', function(data) {
 	console.log(data);
 	t = new track();
 	t.beat(4);
